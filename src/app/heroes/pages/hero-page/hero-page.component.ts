8858c8004e6871a0f140556ed94c48536dec4b9a
Sebasao <sebas.ao@outlook.com>
2023-07-03 21:43:20 -0500

    New components created in their modules

---
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styleUrls: ['./hero-page.component.scss']
+})
+export class HeroPageComponent {
+
+}
